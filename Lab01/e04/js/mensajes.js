feae548381980ccadcc7d7943a50e0eeb072fa78
Jesus Canales <jesuscanales@outlook.com>
2023-05-27 17:18:04 -0500

    Obteniendo datos desde el prompt a la web

---
+++ Lab01/e04/js/mensajes.js
@@ -0,0 +1,12 @@
+// Pedimos los datos a través del prompt
+let nombrePersona = prompt("Ingresa tu nombre: ");
+
+document.write("<h1>Bienvenido a Cibertec</h1>");
+
+// Visualizamos lo ingresado en la página web
+document.write('<h3>' + "Nombre: " + nombrePersona + '</h3>');
+
+// Visualice los apellidos
+
+
+// Visualice el nombre del curso
